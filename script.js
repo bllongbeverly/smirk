e6f3e06e754e1840d554f8652a307c6819158c0c
ActivePatience <124790105+ActivePatience@users.noreply.github.com>
2023-03-29 20:44:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+{
+  var key = "EsYRiuO5UyJ3IGNWDCggwH54klr9JIi8";
+}
+
+
+var ticker = "AAPL";
+
+
+var url = "https://api.polygon.io/v2/aggs/ticker/" + ticker + "/range/1/day/2023-01-09/2023-03-09?adjusted=true&sort=asc&limit=120&apiKey=" + key;
+
+
+
+/*
+function unixToTimestamp(unix) {
+
+  var timestamp = unix*1000;
+  var date = "";
+
+  date = "Date: "+ timestamp.getDate()+
+           "/"+(timestamp.getMonth()+1)+
+           "/"+timestamp.getFullYear()+
+           " "+timestamp.getHours()+
+           ":"+timestamp.getMinutes()+
+           ":"+timestamp.getSeconds();
+
+}
+*/
+
+var array = [2];
+
+function parse(data) {
+  console.log(data);
+  array[0] = data.results[0].t;
+  console.log(array[0]);
+}
+
+function displayPrice() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => parse(data));
+}
+
