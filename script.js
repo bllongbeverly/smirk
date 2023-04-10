dcdc247d3ed33ae3ab87aa771355bd26c3bcd5ef
ActivePatience <124790105+ActivePatience@users.noreply.github.com>
2023-04-10 17:49:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function returnStockData(ticker) {
   }
 
   var today = new Date();
-  var dd = String(today.getDate() - 1).padStart(2, "0");
+  var dd = String(today.getDate() - 5).padStart(2, "0");
   var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   var yyyy = today.getFullYear();
   today = yyyy + "-" + mm + "-" + dd;
